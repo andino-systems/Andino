bb7a4533ff08c8115ee50a5ba435e927ef88bbcd
Chris <christian.drotleff@clearsystems.de>
2020-09-07 14:02:55 +0200

    andino sms

--- Andino-Common/src/NodeRed/AndinoSMS/node-red-contrib-andino-sms/smsChecker/smsChecker.js
+++ Andino-Common/src/NodeRed/AndinoSMS/node-red-contrib-andino-sms/smsChecker/smsChecker.js
@@ -3,20 +3,19 @@ module.exports = function(RED) {
 	function smsChecker(config) {
         RED.nodes.createNode(this,config);
         this.on("input", function (msg) {
-            checkSms(msg);
-        });
-		
-        //set modem to text mode
-		msg.payload="at+cmgf=1";
-        this.send(msg);
-        
-
-        //send sms check message
-        setTimeout(function(){
-		    msg.payload="at+cmgl=\"REC UNREAD\"";
+            
+			//set modem to text mode
+	        msg.payload="at+cmgf=1";
             this.send(msg);
-            return null;
-        }, 500);
+
+            //send sms check message
+            setTimeout(function(){
+	    	    msg.payload="at+cmgl=\"REC UNREAD\"";
+                this.send(msg);
+                return null;
+            }, 500);
+		
+		});
     }
 RED.nodes.registerType("smsChecker",smsChecker);
 }
