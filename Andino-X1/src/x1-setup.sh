eba738bbc0cd259fcd0002c86ffd637e88ea848c
Chris <christian.drotleff@clearsystems.de>
2021-03-10 12:53:29 +0100

    Update x1-setup.sh

--- Andino-X1/src/x1-setup.sh
+++ Andino-X1/src/x1-setup.sh
@@ -6,58 +6,62 @@ printf "3..."
 sleep 1
 printf "2..."
 sleep 1
-printf "1..."
+printf "1...\n"
 sleep 1
+
+cd /home/pi
+mkdir tmp
 
 # update & upgrade
 printf "[X1 Setup] Updating & upgrading packages...\n"
-sleep 1
+sleep 2
 
-printf "\n"
 sudo apt-get update
 sudo apt-get upgrade -y
 
 # install software
 printf "[X1 Setup] Installing software...\n"
-sleep 1
+sleep 2
 sudo apt-get install -y minicom git python python-pip python3 python3-pip
 ### i2c-tools is installed in RTC section
 
 # edit system files
 printf "[X1 Setup] Setting system settings...\n"
-sleep 1
+sleep 2
 
 ## edit /boot/config.txt
 printf "Enabling UART in /boot/config.txt..."
 
-echo "" >> /boot/config.txt
-sudo echo "enable_uart=1" >> /boot/config.txt
-sudo echo "dtoverlay=disable-bt" >> /boot/config.txt
-sudo echo "dtoverlay=miniuart-bt" >> /boot/config.txt
+echo "" | sudo tee -a /boot/config.txt
+echo "enable_uart=1" | sudo tee -a /boot/config.txt
+echo "dtoverlay=disable-bt" | sudo tee -a /boot/config.txt
+echo "dtoverlay=miniuart-bt" | sudo tee -a /boot/config.txt
 
-printf "done.\n"
+
+printf "...done\n"
 
 ## edit /boot/cmdline.txt
 
 printf "Disabling console on /dev/serial0..."
 
-sudo cut -d ' ' -f 3- < /boot/cmdline.txt >> /boot/cmdline-new.txt
+touch tmp/cmdline.txt
+sudo cut -d ' ' -f 3- < /boot/cmdline.txt >> tmp/cmdline.txt
 sudo rm /boot/cmdline.txt
-sudo mv /boot/cmdline-new.txt /boot/cmdline.txt
+sudo mv tmp/cmdline-new.txt /boot/cmdline.txt
 
 printf "done.\n"
 
 # configure RTC
 printf "[X1 Setup] Setting up RTC...\n"
-sleep 1
+sleep 2
 
 printf "Enabling rtc in /boot/config.txt..."
 
-sudo echo "" >> /boot/config.txt
-sudo echo "dtparam=i2c_arm=on" >> /boot/config.txt
-sudo echo "dtoverlay=i2c-rtc,ds3231" >> /boot/config.txt
+echo "" | sudo tee -a /boot/config.txt
+echo "dtparam=i2c_arm=on" | sudo tee -a /boot/config.txt
+echo "dtoverlay=i2c-rtc,ds3231" | sudo tee -a /boot/config.txt
 
-printf "done.\n"
+printf "...done.\n"
 
 printf "Installing necessary packages...\n"
 sudo apt-get install -y i2c-tools
@@ -77,7 +81,7 @@ wget 'https://raw.githubusercontent.com/andino-systems/Andino/master/Andino-Comm
 # install log2ram
 
 printf "[X1 Setup] Setting up Log2Ram...\n"
-sleep 1
+sleep 2
 
 cd /home/pi/
 git clone https://github.com/azlux/log2ram.git
@@ -88,7 +92,7 @@ sudo ./log2ram/install.sh
 # install node.js & node-red
 
 printf "[X1 Setup] Setting up NodeJS & NodeRed...\n"
-sleep 1
+sleep 2
 
 printf "Starting installation. PLEASE CONFIRM WITH 'y' IF PROMPTED.\n"
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-install --confirm-pi
@@ -110,7 +114,7 @@ cd ~
 # install andinopy
 
 printf "[X1 Setup] Setting up Andino Python Library...\n"
-sleep 1
+sleep 2
 
 ## download and unzip
 sudo pip3 install wheel
@@ -122,9 +126,10 @@ rm andinopy.zip
 
 ## setup
 sudo python3 setup.py bdist_wheel
-sudo pip3 install dist/package.whl
+sudo pip3 install dist/andinopy-0.1-py3-none-any.whl
 
 # finish and remove script
 
 printf "[X1 Setup] Setup complete! Please reboot to finish.\n"
+rmdir /home/pi/tmp
 rm /home/pi/x1-setup.sh
